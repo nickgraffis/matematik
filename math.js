ee6068a3803478323a38bda26bb08f4cf9131292
Nick Graffis <nicholasgraffis@gmail.com>
2020-09-26 16:50:43 -0700

    Added several comments

--- math.js
+++ math.js
@@ -1,12 +1,11 @@
 module.exports = {
     euclideanDistance : euclideanDistance,
-    rangeOf : rangeOf,
-    rangesOf: rangesOf,
-    meanDataPoint: meanDataPoint,
     mean: mean,
+    meanPoint: meanPoint,
+    rangeOf: rangeOf,
+    rangesOf: rangesOf
     randomIntBetween: randomIntBetween,
 };
-
 
 function euclideanDistance(a, b) {
     if (a.length != b.length) {
@@ -20,36 +19,56 @@ function euclideanDistance(a, b) {
     return Math.sqrt(sum);
 }
 
-function rangeOf(data) {
-	return data.reduce(function(total, current) {
-		if (current < total.min) { total.min = current; }
-		if (current > total.max) { total.max = current; }
-		return total;
-	}, {min: data[0], max: data[0]});
-}
-
-function rangesOf(data) {
-	var ranges = [];
-	for (let i = 0; i < data[0].length; i++) {
-		ranges.push(rangeOf(data.map(x => x[i])));
-	}
-	return ranges;
-}
-
-function meanDataPoint(data) {
-	var point = [];
-	if (data.length != 0) {
-		for (let i = 0; i < data[0].length; i++) {
-			point.push(mean(data.map(x => x[i])));
-		}
-	}
-	return point;
-}
-
-function mean(data) {
-	return data.reduce((total, current) => total += current, 0) / data.length;
-}
-
 function randomIntBetween(a, b) {
     return Math.floor(Math.random() * (b - a + 1)) + a;
+}
+
+/**
+* Calculates the mean value of a one-dimensional dataset
+* @param {Array} data - data set
+* @return {Number} mean value of data set
+*/
+function mean(data) {
+    return data.reduce((total,current) => total += current, 0) / data.length;
+}
+
+/**
+* Calculates the mean point of an n-dimensional dataset
+* @param {Array} data - data set
+* @return {Array} mean point of data set
+*/
+function meanPoint(data) {
+    var theMeanPoint = [];
+    if (data.length != 0) {
+        for (let i = 0; i < data[0].length; i++) {
+            theMeanPoint.push(mean(data.map(x => x[i])));
+        }
+    }
+    return theMeanPoint;
+}
+
+/**
+* Calculates the range of a one-dimensional data set
+* @param {Array} data - data set
+* @return {Number} range - range of the data set
+*/
+function rangeOf(data) {
+    return data.reduce(function(total,current) {
+        if (current < total.min) { total.min = current; }
+        if (current > total.max) { total.max = current; }
+        return total;
+    }, {min: data[0], max: data[0]});
+}
+
+/**
+* Calculates the ranges of each 'component' in an n-dimensional data set
+* @param {Array} data - data set
+* @return {Number} range - range of the data set
+*/
+function rangesOf(data) {
+    var ranges = [];
+    for (let i = 0; i < data[0].length; i++) {
+        ranges.push(rangeOf(data.map(x => x[i])));
+    }
+    return ranges;
 }
